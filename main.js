061598d5abb89669bfb3196f4fb55d8cf95eae7b
Saengchailnwza <grgamerkung@gmail.com>
2025-06-26 23:36:37 +0700

    3.1-3.6

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ const {createApp, ref } = Vue
 createApp({
     setup(){
         const product = ref('Boots')
-        const description = ref('helooo')
+        const image = ref('./assets/images/socks_blue.jpg')
+        const link = ref('https://www.camt.cmu.ac.th.')
 
         return {
             product,
-            description
+            image,
+            link
         }
     }
     
 }).mount('#app')
+
 
 
