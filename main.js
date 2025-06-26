fb039ef4fd781d9c6b9032849466f59fc3b02311
Saengchailnwza <grgamerkung@gmail.com>
2025-06-26 23:57:36 +0700

    4.1-4.9

--- main.js
+++ main.js
@@ -5,11 +5,18 @@ createApp({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_blue.jpg')
         const link = ref('https://www.camt.cmu.ac.th.')
+        const inStock = ref(true)
+        const onSale = ref(true)
+        const inventory =ref(100)
 
         return {
             product,
             image,
-            link
+            link,
+            inStock,
+            inventory,
+            onSale
+
         }
     }
     
